e7571b8642327134bbd404d9d44e9b8546517b93
Hussein039 <84193192+Hussein039@users.noreply.github.com>
2023-11-16 19:41:57 -0500

    my commit

---
+++ src/pages/about.js
@@ -0,0 +1,108 @@
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import profile from "../../public/images/profile/developer-pic-1.png";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import { useEffect, useRef } from "react";
+import Skills from "@/components/Skills";
+import Experience from "@/components/Experience";
+import Education from "@/components/Education";
+import AnimatedText from "@/components/AnimatedText";
+import TransitionEffect from "@/components/TransitionEffect";
+
+function AnimatedNumberFramerMotion({ value }) {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [motionValue, value, isInView]);
+
+  useEffect(
+    () =>
+      springValue.on("change", (latest) => {
+        if (ref.current && latest.toFixed(0) <= value) {
+          ref.current.textContent = latest.toFixed(0);
+        }
+      }),
+    [springValue, value]
+  );
+
+  return <span ref={ref} />;
+}
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>About Me</title>
+        <meta name="description" content="Learn more about CodeBucks, a Next.js developer with a passion for 
+        creating innovative solutions. Discover tips for building a developer portfolio and insights on 
+        full-stack development, front-end development, and back-end development." />
+      </Head>
+      <TransitionEffect />
+      <main
+        className={`flex  w-full flex-col items-center justify-center dark:text-light`}
+      >
+        <Layout className="pt-16">
+          <AnimatedText
+            text="About Me!"
+            className="mb-16 !text-8xl !leading-tight lg:!text-7xl sm:!text-6xl xs:!text-4xl sm:mb-8"
+          />
+
+          <div className="grid w-full grid-cols-8 gap-16 sm:gap-8">
+            <div className="col-span-3 flex flex-col items-start justify-start xl:col-span-4 md:order-2 
+            md:col-span-8">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75 dark:text-light/75">
+                BIOGRAPHY
+              </h2>
+              <p className="font-medium">
+                  Hello, I'm <strong>Hussein</strong>, a skilled web developer dedicated to crafting visually stunning,
+                  functional, and user-centric digital experiences.
+                  I continually seek innovative approaches to bring my clients' visions to vibrant life.
+                </p>
+                <p className="my-4 font-medium">
+                  I firmly believe that design transcends mere aesthetics 
+                  – it's about solving challenges and
+                  fashioning instinctive, delightful experiences for users.
+                </p>
+                <p className="font-medium">
+                  Whether I'm immersed in website, mobile app, or other digital product development,
+                  I infuse each project with my unwavering commitment to design excellence and user-focused
+                  perspective. I eagerly anticipate the opportunity to contribute my skills and enthusiasm
+                  to your upcoming venture.
+                </p>
+
+            </div>
+            <div className="relative col-span-3 h-max rounded-2xl border-2 border-solid border-dark 
+            bg-light p-8 dark:border-light dark:bg-dark
+            xl:col-span-4 md:col-span-8 md:order-1
+            ">
+              <div
+                className="absolute  top-0 -right-3 -z-10 h-[103%] w-[102%]  rounded-[2rem] rounded-br-3xl 
+                bg-dark
+        dark:bg-light  "
+              />
+              <Image
+                className="h-auto w-full rounded-2xl"
+                src={profile}
+                alt="Codebucks"
+                sizes="(max-width: 768px) 100vw,
+              (max-width: 1200px) 50vw,
+              33vw"
+                priority
+              />
+            </div>
+          </div>
+
+          <Skills />
+          <Experience />
+          <Education />
+        </Layout>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,79 @@
+import AnimatedText from "@/components/AnimatedText";
+import { HireMe } from "@/components/HireMe";
+import { LinkArrow } from "@/components/Icons";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import profilePic from "../../public/images/profile/developer-pic-1.png";
+import TransitionEffect from "@/components/TransitionEffect";
+
+
+export default function Home() {
+  
+  return (
+    <>
+      <Head>
+        <title>Hussein Portfolio</title>
+        <meta
+          name="description"
+          content="Explore CodeBucks's Next.js developer portfolio and 
+        discover the latest webapp projects and software engineering articles. 
+        Showcase your skills as a full-stack developer and software engineer."
+        />
+      </Head>
+
+      <TransitionEffect />
+      <article
+        className={`flex min-h-screen items-center text-dark dark:text-light sm:items-start`}
+      >
+        <Layout className="!pt-0 md:!pt-16 sm:!pt-16">
+          <div className="flex w-full items-start justify-between md:flex-col">
+            <div className="w-1/2 lg:hidden md:inline-block md:w-full">
+              <Image
+                src={profilePic}
+                alt="CodeBucks"
+                className="h-auto w-full"
+                sizes="100vw"
+                priority
+              />
+            </div>
+            <div className="flex w-1/2 flex-col items-center self-center lg:w-full lg:text-center">
+              <AnimatedText
+                text="Turning ideas into reality with code and design."
+                className="!text-left !text-6xl xl:!text-5xl lg:!text-center lg:!text-6xl md:!text-5xl sm:!text-3xl"
+              />
+              <p className="my-4 text-base font-medium md:text-sm sm:!text-xs">
+              As an adept front-end developer, my commitment lies in transforming concepts into inventive web applications. Delve into my recent projects, highlighting my proficiency in React.js and web development.
+              </p>
+              <div className="mt-2 flex items-center self-start lg:self-center">
+                <Link
+                  href="/public/Hussein Resumedocx.docx"
+                  target={"_blank"}
+                  className={`flex items-center rounded-lg border-2 border-solid bg-dark p-2.5 px-6 text-lg font-semibold
+                  capitalize text-light hover:border-dark hover:bg-transparent hover:text-dark 
+                  dark:bg-light dark:text-dark dark:hover:border-light dark:hover:bg-dark dark:hover:text-light
+                  md:p-2 md:px-4 md:text-base
+                `}
+                  download
+                >
+                  Resume <LinkArrow className="ml-1 !w-6 md:!w-4" />
+                </Link>
+
+                <Link
+                  href="mailto:husseinsaad123123@gmail.com"
+                  className="ml-4 text-lg font-medium capitalize text-dark underline 
+                  dark:text-light md:text-base"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Layout>
+
+        <HireMe />
+      </article>
+    </>
+  );
+}
